1677138242aa3dd5ba7319b85f7a061e9c2f9788
emvalencaf <edsonmvf@gmail.com>
2022-11-01 01:11:12 -0300

    Update Register.page.js

--- frontend/src/pages/Auth/Register.page.js
+++ frontend/src/pages/Auth/Register.page.js
@@ -15,10 +15,23 @@ import './Auth.css';
 
 const Register = () => {
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
 
   const handleSubmit = (e) => {
 
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmpassword
+    };
+
+    console.log(user);
 
   };
 
@@ -34,21 +47,29 @@ const Register = () => {
           type="text"
           name="name"
           placeholder="Nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input
           type="email"
           name="email"
           placeholder='E-mail'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           name="password"
           placeholder='Senha'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <input
           type="password"
           name="confirmpassword"
           placeholder='Confirme a senha'
+          value={confirmpassword}
+          onChange={(e) => setConfirmpassword(e.target.value)}
         />
         <button type='submit'>Cadastrar</button>
       </form>
